17d95c5280369e81549136e92f770b27b7052612
Riccardo Miccini <miccio-dk@users.noreply.github.com>
2021-06-01 17:34:00 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,8 +13,8 @@ aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}" > ~/.aws/credentials
 # comando per assegnare layer alla funzione lambda
 
 aws lambda update-function-configuration \
-  --function-name ${AWS_LAMBDA_ARN} \
-  --layers ${LAYER_NAME} \
+  --function-name ${AWS_LAMBDA_NAME} \
+  --layers ${AWS_LAYER_ARN} \
   --region ${AWS_REGION}
 
 rm -rf ~/.aws
